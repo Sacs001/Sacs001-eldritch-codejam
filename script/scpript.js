985f672d9d9dbe557c257e60b0f7ed7dd73b94b6
Sacs001 <github.com/Sacs001/Sacs001.git>
2022-08-27 00:33:39 +0300

    reached 80%

--- script/scpript.js
+++ script/scpript.js
@@ -13,9 +13,27 @@ let brownDiff = [];
 let greenDiff = [];
 let blueDiff = [];
 let scoupCard = [];
-console.log(cards)
-let chooseMonsterDifficulty = document.querySelector('.alert');
+let greenCard = '';
+let blueCard = '';
+let brownCard = '';
+let massiveWithCardMediumLevelBrown = [];
+let massiveWithCardMediumLevelBlue = [];
+let massiveWithCardMediumLevelGreen = [];
 
+cards[0].forEach((el) => {
+    if (el.difficulty == "medium") massiveWithCardMediumLevelBrown.push(el)
+})
+
+cards[1].forEach((el) => {
+    if (el.difficulty == "medium") massiveWithCardMediumLevelBlue.push(el)     //список с картами среднего уровня
+})
+cards[2].forEach((el) => {
+    if (el.difficulty == "medium") massiveWithCardMediumLevelGreen.push(el)
+})
+
+console.log(ancientsMonster)
+let chooseMonsterDifficulty = document.querySelector('.alert');
+//нажатие кнопок
 document.addEventListener('click', (event) => {
     switch (event.target.id) {
         case 'azathoth':
@@ -33,7 +51,6 @@ document.addEventListener('click', (event) => {
     }
 
     switch (event.target.id) {
-
         case 'veryEasy':
             infoForShuffleTheDeck.difficulty = event.target.id
             break
@@ -59,17 +76,50 @@ document.querySelector('.btn_start').addEventListener('click', () => {
     if (infoForShuffleTheDeck.god !== '' && infoForShuffleTheDeck.difficulty !== '') {
         ancientsMonster.forEach((el) => {                       //смотрим сколько карт нужно разного цвета
             if (infoForShuffleTheDeck.god == el.id) {
-                let greenCard = el.firstStage.greenCards + el.secondStage.greenCards + el.thirdStage.greenCards
-                let blueCard = el.firstStage.blueCards + el.secondStage.blueCards + el.thirdStage.blueCards
-                let brownCard = el.firstStage.brownCards + el.secondStage.brownCards + el.thirdStage.brownCards
-                console.log(greenCard)
-                console.log(blueCard)
-                console.log(brownCard)
+                greenCard = el.firstStage.greenCards + el.secondStage.greenCards + el.thirdStage.greenCards
+                blueCard = el.firstStage.blueCards + el.secondStage.blueCards + el.thirdStage.blueCards
+                brownCard = el.firstStage.brownCards + el.secondStage.brownCards + el.thirdStage.brownCards
+                // console.log(greenCard)
+                // console.log(blueCard)
+                // console.log(brownCard)
             }
         })
     } else console.log(`Error push info`)
-
+    // console.log(greenCard)
     chooseDifficulty(infoForShuffleTheDeck.difficulty)
+
+    shuffle(greenDiff);
+    shuffle(blueDiff);
+    shuffle(brownDiff);
+
+    let green = addCard(greenCard, greenDiff);
+    let blue = addCard(blueCard, blueDiff);  //массивы для каждого монстра
+    let brown = addCard(brownCard, brownDiff);
+    console.log(greenDiff)
+    if (infoForShuffleTheDeck.difficulty == 'veryEasy') {
+        addMisssingCard(greenCard, greenDiff, massiveWithCardMediumLevelGreen)
+        addMisssingCard(brownCard, brownDiff, massiveWithCardMediumLevelBrown)  //добавление карт, если их не хватает другой сложности
+        addMisssingCard(blueCard, blueDiff, massiveWithCardMediumLevelBlue)
+    } else if (infoForShuffleTheDeck.difficulty == 'veryHard') {
+
+        addMisssingCard(greenCard, greenDiff, massiveWithCardMediumLevelGreen)
+        addMisssingCard(brownCard, brownDiff, massiveWithCardMediumLevelBrown)  //добавление карт, если их не хватает другой сложности
+        addMisssingCard(blueCard, blueDiff, massiveWithCardMediumLevelBlue)
+    }
+
+    // function addMisssingCard(colorCard, colorDiff, additionalMassiv) {             // проверка хватает ли карт для замешивания
+
+    //     if (colorDiff.length < colorCard.length) {
+    //         let i = colorCard.length - colorDiff.length
+    //         for (i; i > 0; i--) {
+    //             console.log(additionalMassiv)
+    //             colorDiff.push(shuffle(additionalMassiv).pop);
+    //         }
+    //     }
+    // }
+    // console.log(green);
+    // console.log(blue);
+    // console.log(brown);
 })
 
 function stageOfDifficulty(arr, numArr, difficulty) {
@@ -79,6 +129,9 @@ function stageOfDifficulty(arr, numArr, difficulty) {
 //переделать на функции во всех уровнях сложности!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function chooseDifficulty(difficulty) {
     // let allCard = [];
+    brownDiff = [];
+    blueDiff = [];
+    greenDiff = [];
     switch (difficulty) {
 
         case 'veryEasy':
@@ -137,10 +190,43 @@ function chooseDifficulty(difficulty) {
             break
     }
     console.log(brownDiff)
-    console.log(blueDiff)
+    console.log(blueDiff)//                
     console.log(greenDiff)
-    brownDiff = []
-    blueDiff = []
-    greenDiff = []
+
 }
+
+function addCard(numCardForPush, arrCard) {    //делаем массив с обработанными картами
+    scoupCard = [];
+    for (let i = numCardForPush; numCardForPush > 0; numCardForPush--) {
+        scoupCard.push(arrCard.pop());
+    }
+    return scoupCard
+}
+
+
+function shuffle(arr) {    //метод сортировки массива
+    let j;
+    let temp;
+    for (var i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+function addMisssingCard(colorCard, colorDiff, additionalMassiv) {             // проверка хватает ли карт для замешивания
+    console.log(colorDiff.length)
+    console.log(colorCard.length)
+    if (colorDiff.length < colorCard.length) {
+        let i = colorCard.length - colorDiff.length
+        console.log(i)
+        for (i; i > 0; i--) {
+            // console.log(additionalMassiv)
+            colorDiff.push(shuffle(additionalMassiv).pop);
+        }
+    }
+}
+
 
